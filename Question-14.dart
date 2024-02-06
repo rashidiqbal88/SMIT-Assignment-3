6bc17d03f2031077e514f38bf5065b7540ad5df4
rashidiqbal88 <rashidiqbal.developer@gmail.com>
2024-02-06 15:14:28 +0500

    Q:14 update code...

--- Question-14.dart
+++ Question-14.dart
@@ -2,11 +2,19 @@ void main(){
   print("Q.14: Write a Dart code that takes in a list of integers and prints a new list with the elements sorted in ascending order. The original list should remain unchanged.");
 
   List<int> originalList = [18,11,1,10,0,15,8,3,14,2];
-  List<int> secondList = [];
+//  List<int> secondList = [];
+  // secondList.addAll(originalList);  
+  // secondList.sort();
+  // print(secondList);
 
-  secondList.addAll(originalList);  
-  secondList.sort();
-  print(secondList);
+   var result = originalList.sublist(1);
+   result.sort();
 
+   print("This is Orignal List: $originalList");
+
+   print("");
+
+   print("This is New List: $result");
 
 }
+
